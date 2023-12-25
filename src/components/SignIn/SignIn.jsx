bad216e51ebb29ec4ecd42263fc7051b933ac3b3
rthinh2002 <rthinh2002@gmail.com>
2023-12-26 09:38:45 +1030

    Remove flying object

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import { Container, Grid } from '@mui/material';
-import Polygon from '../PolygonObject/Polygon';
+import { Grid } from '@mui/material';
 
 const SignIn = () => {
   return (
-    <Container maxWidth="xl" sx={{ display: 'flex', justifyContent: 'flex-end', backgroundColor: "#073138", height: '100vh' }}>
-        <Polygon type="Triangle" top="15%" right="25%" rotate="30deg"/>
-        <Polygon type="Circle" top="75%" right="45%" rotate="30deg"/>
-    </Container>
+    <Grid container component="main" sx={{ height: "100vh", backgroundColor: "#073138" }}>
+
+    </Grid>
   )
 }
 
